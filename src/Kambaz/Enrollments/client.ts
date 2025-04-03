68a9fee2fa0610d9c3b65b91431e421814fbf3ca
Caden Elghazal <cadenelg@gmail.com>
2025-04-03 10:53:57 -0400

    added cookies to all client

--- src/Kambaz/Enrollments/client.ts
+++ src/Kambaz/Enrollments/client.ts
@@ -1,20 +1,21 @@
 import axios from "axios";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
+const axiosWithCredentials = axios.create({ withCredentials: true });
 
 export const unenroll = async (userId: string, courseId: string) => {
-  const response = await axios.delete(`${ENROLLMENTS_API}/${userId}/${courseId}`);
+  const response = await axiosWithCredentials.delete(`${ENROLLMENTS_API}/${userId}/${courseId}`);
   return response.data;
 };
 
 export const enroll = async (userId: string, courseId: string) => {
-  const response = await axios.post(`${ENROLLMENTS_API}`, { userId, courseId });
+  const response = await axiosWithCredentials.post(`${ENROLLMENTS_API}`, { userId, courseId });
   console.log(response.data);
   return response.data;
 }
 
 export const findEnrollments = async () => {
-  const { data } = await axios.get(ENROLLMENTS_API);
+  const { data } = await axiosWithCredentials.get(ENROLLMENTS_API);
   console.log(data);
   return data;
 }
