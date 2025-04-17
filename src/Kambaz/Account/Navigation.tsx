888bf31fbcf029b84c7761464a6f037dfc30afa3
Caden Elghazal <cadenelg@gmail.com>
2025-04-17 02:10:01 -0400

    initial updates before web service

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((val: string) => (
         <>
           <Link
             to={`/Kambaz/Account/` + val}
-            className="list-group-item active border border-0"
+            className={`list-group-item ${active(val)} border border-0`}
           >
             {" "}
             {val}{" "}
@@ -17,6 +20,15 @@ export default function AccountNavigation() {
           <br />
         </>
       ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          to={`/Kambaz/Account/Users`}
+          className={`list-group-item ${active("Users")} border border-0`}
+        >
+          {" "}
+          Users{" "}
+        </Link>
+      )}
     </div>
   );
 }
